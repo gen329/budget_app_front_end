8db58dbb511bcf7de5c44faba78190ac668491c1
Genesis Lara <genesislara@pursuit.org>
2023-10-22 18:52:03 -0400

    debugging new form

--- src/Components/TransactionNewForm.jsx
+++ src/Components/TransactionNewForm.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Link, useParams, useNavigate } from "react-router-dom";
 const API = import.meta.env.VITE_BASE_URL
 
@@ -12,74 +12,86 @@ function TransactionNewForm() {
     category: "",
   });
 
-    const navigate = useNavigate();
-    const handleTextChange = (event) => {
-      setTransaction({ ...transaction, [event.target.id]: event.target.value });
-    };
-    const addTransaction = () => {
+  const navigate = useNavigate();
+
+  const handleTextChange = (event) => {
+    const { id, value } = event.target;
+    setTransaction((prevTransaction => ({ ...prevTransaction, [id]: value })));
+  };
+
+  const addTransaction = async(transaction) => {
+    try {
       const httpOptions = {
-        "method" : "POST",
-        "body" : JSON.stringify(transaction),
-        "headers" : {
-          "Content-type" : "application/json"
-        }
-      }
-      fetch(`${API}/transactions`, httpOptions)
-        .then((response) => {
-          console.log(response)
-          alert(`${transaction.item_name} was added to the budget!`)
-          navigate('/transactions');
-        })
-        .catch((error) => console.error(error))
+        "method": "POST",
+        "body": JSON.stringify(transaction),
+        "headers": {
+          "Content-type": "application/json"
+        },
+      };
+
+      const response = await fetch(`${API}/transactions`, httpOptions);
+      if (!response.ok) { throw new Error("Network response failed!!") }
+
+      const data = await response.json();
+      console.log(data);
+      alert(`${transaction.item_name} was successfully added to the budget!`);
+      navigate('/transactions');
+    } catch (error) {
+      console.error(error);
     }
+  };
+
     const handleSubmit = (event) => {
       event.preventDefault();
       addTransaction();
     };
+    
+    console.log(transaction.item_name)
     return (
-    <div className="New">
-      <form onSubmit={handleSubmit}>
-     
+      <div className="New">
+    <form onSubmit={handleSubmit}>
+
       <label htmlFor="date">Date:</label>
-        <input
-          id="date"
-          value={transaction.date}
-          type="number"
-          onChange={ handleTextChange }
-          placeholder="date"
-          required
+      <input
+        id="date"
+        value={transaction.date}
+        type="text"
+        onChange={handleTextChange}
+        placeholder="date"
+        required
         />
-
-        <label htmlFor="name">Name:</label>
-        <input
-          id="name"
-          type="text"
-          value={transaction.item_name}
-          placeholder="name"
-          onChange={handleTextChange}
+      <br />
+      <label htmlFor="name">Name:</label>
+      <input
+        id="name"
+        type="text"
+        value={transaction.item_name}
+        placeholder="name"
+        onChange={handleTextChange}
         />
-
-        <label htmlFor="amount">Amount:</label>
-        <input
-          id="amount"
-          type="number"
-          value={transaction.amount}
-          placeholder="amount"
-          onChange={handleTextChange}
+      <br />
+      <label htmlFor="amount">Amount:</label>
+      <input
+        id="amount"
+        type="number"
+        value={transaction.amount}
+        placeholder="amount"
+        onChange={handleTextChange}
         />
-
-        <label htmlFor="from">From:</label>
-        <input
-          id="from"
-          type="text"
-	        value={transaction.transaction_from}
-          onChange={handleTextChange}
+      <br />
+      <label htmlFor="from">From:</label>
+      <input
+        id="from"
+        type="text"
+        value={transaction.transaction_from}
+        onChange={handleTextChange}
         />
-        <br />
-        <input type="submit" />
-      </form>
-    </div>
-  );
+      <br />
+      <input type="submit" />
+    </form>
+  </div>
+);
 };
+
 
 export default TransactionNewForm;
