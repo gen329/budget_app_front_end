35e2fbcb3c53cde746a80dd1b785dc786f6134e0
Genesis Lara <genesislara@pursuit.org>
2023-10-22 11:46:21 -0400

    connects to express

---
+++ src/Components/TransactionDetails.jsx
@@ -0,0 +1,8 @@
+import { useState, useEffect } from "react";
+import { Link, useParams, useNavigation } from "react";
+const API = import.meta.env.VITE_BASE_URL ;
+
+
+
+
+export default TransactionDetails;
